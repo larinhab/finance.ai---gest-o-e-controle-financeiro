c71e52475739997c7ab25cfe03e37659e73864ce
Lara <lara.viana@1app.com.br>
2024-11-05 11:16:39 -0300

    chore: add shadcn setup + button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return <h1 className="p-5 text-red-500"> Hellow </h1>;
+  return;
+
+  <Button>Hello</Button>;
 };
 
 export default Home;
