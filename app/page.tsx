e3d96615d052effff4aaa9c60819cf3aa2c55e51
Lara <larabviana@outlook.com>
2024-11-05 14:01:22 -0300

    chore: add husky, git hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 const Home = async () => {
   const { userId } = await auth();
   if (userId) {
-    redirect("/");
+    redirect("/login");
   }
 
   return (
